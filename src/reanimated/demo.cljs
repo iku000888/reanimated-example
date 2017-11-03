01e59d21ac1e0b6bab54220312c1609f64840b48
Ikuru K <kanumaiku@gmail.com>
2017-11-03 18:15:46 +0900

    Make things fancy

--- src/reanimated/demo.cljs
+++ src/reanimated/demo.cljs
@@ -3,6 +3,30 @@
    [reagent.core :as r]
    [reanimated.core :as rc]))
 
+(defn item [s stuff]
+  (let [size (r/atom 40)
+        spr (rc/spring size)]
+    (fn []
+      [:div {:on-click #(swap! stuff
+                               (fn [stuff]
+                                 (assoc stuff s false)))
+             :style {:font-size (str @spr "px")
+                     :margin "10px"
+                     :cursor "pointer"}}
+       s
+       [rc/timeline
+        #(reset! size 100)
+        200
+        #(reset! size 40)]
+       (when-not (get @stuff s)
+         [rc/timeline
+          #(reset! size 100)
+          400
+          #(reset! size 20)
+          200
+          #(swap! stuff dissoc s)
+          ])])))
+
 
 (defn main []
   (let [size (r/atom 40)
@@ -12,18 +36,37 @@
                         :velocity .8
                         :mass 2
                         :stiffness 0.13
-                        :damping .17})]
+                        :damping .17})
+        stuff (r/atom {"foo" true
+                       "bar" true
+                       "baz" true})]
     (fn []
       [:div
-       {:on-click #(swap! tl not)
-        :style {:font-size (str @spr "px")}}
-       "click me!"
-       (when @tl
-         [rc/timeline
-          #(reset! size 100)
-          200
-          #(reset! size 40)
-          #(reset! tl false)])])))
+       [:span {:on-click #(swap! tl not)
+               :style {:font-size (str @spr "px")}}
+        "click me!"
+        (when @tl
+          [rc/timeline
+           #(reset! size 100)
+           200
+           #(reset! size 40)
+           #(reset! tl false)])]
+       [:hr]
+       [:div
+        [:input {:type "button" :value  "Add stuff"
+                 :on-click #(swap! stuff
+                                   (fn [stuff]
+                                     (assoc stuff (str (gensym "stuff")) true)))}]
+        [:input {:type "button" :value  "Remove stuff"
+                 :on-click #(swap! stuff
+                                   (fn [stuff]
+                                     (assoc stuff (ffirst stuff) false)))}]
+        [:div {:style {:display "flex"
+                       :flex-wrap "wrap"}}
+         (->> @stuff
+              sort
+              (map (fn [[s]] ^{:key s} [item s stuff]))
+              doall)]]])))
 
 (r/render [main]
           (js/document.getElementById "app"))
